5f18369405a63a9c2f34e381701aaed4a9ddeca9
Timothy Randall <rantowork@gmail.com>
2016-01-03 22:18:33 -0600

    Initial commit - fresh, default project

---
+++ DapperIdentity/DapperIdentity.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DapperIdentity.Web.MvcApplication" Language="C#" %>
---
+++ DapperIdentity/DapperIdentity.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DapperIdentity.Web.Startup))]
+namespace DapperIdentity.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
