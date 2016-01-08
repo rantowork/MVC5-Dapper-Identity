5b6f4b5e77932ec868589f08d8e874d044f31748
Timothy Randall <rantowork@gmail.com>
2016-01-07 21:53:41 -0600

    Adding create scripts for user table and external logins

---
+++ DapperIdentity/DapperIdentity.Data/Scripts/CreateExternalLoginsTable.sql
@@ -0,0 +1,12 @@
+﻿SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ExternalLogins] (
+    [ExternalLoginId] UNIQUEIDENTIFIER NOT NULL,
+    [UserId]          NVARCHAR (100)   NOT NULL,
+    [LoginProvider]   NVARCHAR (MAX)   NOT NULL,
+    [ProviderKey]     NVARCHAR (MAX)   NOT NULL
+);
---
+++ DapperIdentity/DapperIdentity.Data/Scripts/CreateUserTable.sql
@@ -0,0 +1,16 @@
+﻿SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Users] (
+    [Id]                NVARCHAR (100) NOT NULL,
+    [UserName]          NVARCHAR (MAX) NOT NULL,
+    [Nickname]          NVARCHAR (MAX) NOT NULL,
+    [PasswordHash]      NVARCHAR (MAX) NULL,
+    [SecurityStamp]     NVARCHAR (MAX) NULL,
+    [IsConfirmed]       BIT            NOT NULL,
+    [ConfirmationToken] NVARCHAR (MAX) NULL,
+    [CreatedDate]       DATETIME       NOT NULL
+);
