1153bb2786bbcdeff54a6199e30ba3ff62d02022
Timothy Randall <rantowork@gmail.com>
2016-01-13 21:35:46 -0600

    Added some final comments.

--- DapperIdentity/DapperIdentity.Data/Connections/SqlConnectionFactory.cs
+++ DapperIdentity/DapperIdentity.Data/Connections/SqlConnectionFactory.cs
@@ -3,6 +3,10 @@ using DapperIdentity.Core.Interfaces;
 
 namespace DapperIdentity.Data.Connections
 {
+    /// <summary>
+    /// Connection factory responsible for creating a new SqlConnection.  Should be injected into the repository.  I prefer injecting it in a manner in which the 
+    /// connection string can be passed in (in Ninject it is Bind.To.WithConstructorArgument)
+    /// </summary>
     public class SqlConnectionFactory : IConnectionFactory
     {
         private readonly string _connectionString;
